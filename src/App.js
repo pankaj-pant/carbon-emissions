9dd35dc492e513cf8a0c0797cd3246153febecb2
pankaj-pant <pankajpant89@gmail.com>
2020-06-24 13:12:13 +0300

    per capita calculation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [country, setCountry] = useState("")
+  const [population, setPopulation] = useState([])
+  const [emissions, setEmissions] = useState([])
+
+  const handleChange = (event) => {
+    setCountry(event.target.value)
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log("Fetching data for " + country)
+    fetchData()
+  }
+  const fetchData = () => {
+    axios.get(`https://api.worldbank.org/v2/country/${country}/indicator/SP.POP.TOTL?format=json`)
+      .then((response) => {
+        console.log(response.data)
+        const populationArray = response.data[1].map(c => c.value)
+        setPopulation(populationArray)
+      })
+    axios.get(`https://api.worldbank.org/v2/country/${country}/indicator/EN.ATM.CO2E.KT?format=json`)
+    .then((response) => {
+      console.log(response.data)
+      const emissionsArray = response.data[1].map(c => c.value)
+      setEmissions(emissionsArray)
+    })
+  }
+  const result = () => {
+    return (
+      <p>CO2 emissions per capita (2014): {((emissions[5] / population[5]) * 1000).toFixed(1)} tons per capita</p>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CO2 Emissions</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="country">Country: </label>
+        <input type="text" id="country" name="country" placeholder="Finland" onChange={handleChange}></input>
+        <input type="submit" value="submit"></input>
+      </form>
+      {population.length === 0 ? null : result()}
     </div>
-  );
+  )
 }
 
 export default App;
