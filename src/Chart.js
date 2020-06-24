806d0312c6a1af8177deb182afaab83f37b48ea2
pankaj-pant <pankajpant89@gmail.com>
2020-06-24 14:25:55 +0300

    chart overlay fix

--- src/Chart.js
+++ src/Chart.js
@@ -12,20 +12,21 @@ const Chart = ({years, emissions, population}) => {
             type: 'scatter',
             mode: 'lines+markers',
             marker: {color: 'red'},
+            name: 'population'
           },
-          {type: 'bar', x: years, y: emissions, yaxis: 'y2'},
+          {type: 'bar', x: years, y: emissions, yaxis: 'y2', name: 'emissions'},
         ]}
         layout={ {
             width: 1020, 
             height: 640, 
             title: 'Population & Emissions (1970-2014)',
-            yaxis: {title: 'population'},
+            yaxis: {title: 'population',
+            overlaying: 'y2',
+            side: 'right'},
             yaxis2: {
               title: 'emissions in kiloTonnes',
               titlefont: {color: 'rgb(148, 103, 189)'},
-              tickfont: {color: 'rgb(148, 103, 189)'},
-              overlaying: 'y',
-              side: 'right'
+              tickfont: {color: 'rgb(148, 103, 189)'}
             }
         } }
       />
