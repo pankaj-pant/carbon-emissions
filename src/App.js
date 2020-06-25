aad71adde44446b6cb58a976071f66d1630be899
pankaj-pant <pankajpant89@gmail.com>
2020-06-25 10:05:47 +0300

    better readability

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Chart from './Chart'
 
 const App = () => {
   const [country, setCountry] = useState("")
+  const [countryName, setCountryName] = useState("")
   const [population, setPopulation] = useState([])
   const [emissions, setEmissions] = useState([])
   const [years, setYears] = useState([])
@@ -25,6 +26,8 @@ const App = () => {
         setPopulation(populationArray)
         const yearsArray = response.data[1].map(c => c.date)
         setYears(yearsArray)
+        const name = response.data[1][0].country.value
+        setCountryName(name)
       })
     axios.get(`https://api.worldbank.org/v2/country/${country}/indicator/EN.ATM.CO2E.KT?format=json`)
     .then((response) => {
@@ -35,7 +38,13 @@ const App = () => {
   }
   const result = () => {
     return (
-      <p>CO2 emissions per capita (2014): {((emissions[5] / population[5]) * 1000).toFixed(1)} tons per capita</p>
+      <div>
+        <p>CO2 emissions per capita (2014): {countryName} emits {" "}
+         {((emissions[5] / population[5]) * 1000).toFixed(1)} tons of CO2 per capita in a year.
+        </p>
+        <Chart years={years} emissions={emissions} population={population} countryName={countryName}/>
+      </div>
+      
     )
   }
   return (
@@ -47,7 +56,6 @@ const App = () => {
         <input type="submit" value="submit"></input>
       </form>
       {population.length === 0 ? null : result()}
-      <Chart years={years} emissions={emissions} population={population}/>
     </div>
   )
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-const Chart = ({years, emissions, population}) => {
+const Chart = ({years, emissions, population, countryName}) => {
 
     return (
       <Plot
@@ -14,12 +14,13 @@ const Chart = ({years, emissions, population}) => {
             marker: {color: 'red'},
             name: 'population'
           },
-          {type: 'bar', x: years, y: emissions, yaxis: 'y2', name: 'emissions'},
+          {type: 'bar', x: years, y: emissions, yaxis: 'y2', name: 'emissions', marker: {color: 'grey'}},
         ]}
         layout={ {
             width: 1020, 
-            height: 640, 
-            title: 'Population & Emissions (1970-2014)',
+            height: 640,
+            legend: {"orientation": "h"}, 
+            title: `Population & Emissions for ${countryName} (1970-2014)`,
             yaxis: {title: 'population',
             overlaying: 'y2',
             side: 'right'},
