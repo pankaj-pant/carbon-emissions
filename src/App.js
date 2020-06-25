3e3671e4c38cb566acfa3fb9aeeb2e1074dcb02a
pankaj-pant <pankajpant89@gmail.com>
2020-06-25 12:36:21 +0300

    catches errors

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const App = () => {
   const [population, setPopulation] = useState([])
   const [emissions, setEmissions] = useState([])
   const [years, setYears] = useState([])
+  const [error, setError] = useState("")
 
   const handleChange = (event) => {
     setCountry(event.target.value)
@@ -19,6 +20,11 @@ const App = () => {
     fetchData()
   }
   const fetchData = () => {
+    setCountryName("")
+    setPopulation([])
+    setEmissions([])
+    setYears([])
+    setError("")
     axios.get(`https://api.worldbank.org/v2/country/${country}/indicator/SP.POP.TOTL?format=json`)
       .then((response) => {
         console.log(response.data)
@@ -29,11 +35,17 @@ const App = () => {
         const name = response.data[1][0].country.value
         setCountryName(name)
       })
+      .catch(err => {console.log(err)})
     axios.get(`https://api.worldbank.org/v2/country/${country}/indicator/EN.ATM.CO2E.KT?format=json`)
     .then((response) => {
       console.log(response.data)
       const emissionsArray = response.data[1].map(c => c.value)
       setEmissions(emissionsArray)
+    })
+    .catch(err => {
+      console.log(err)
+      setError(`The World Bank API does not have the requested data on ${country} at the moment. 
+      Please try searching for another country.`)
     })
   }
   const result = () => {
@@ -52,9 +64,11 @@ const App = () => {
       <h1>CO2 Emissions</h1>
       <form onSubmit={handleSubmit}>
         <label htmlFor="country">Country: </label>
-        <input type="text" id="country" name="country" placeholder="Finland" onChange={handleChange}></input>
+        <input type="text" id="country" name="country" placeholder="FI for Finland" onChange={handleChange}></input>
         <input type="submit" value="submit"></input>
+        <a href="https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes" target="__blank">Country codes</a>
       </form>
+      {error.length === 0 ? null : <p> {error} </p>}
       {population.length === 0 ? null : result()}
     </div>
   )
